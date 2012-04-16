51457d1b92490d3716ff68abae0d38601a7b5f5b
Konstantin Savelyev <konstantin.savelyev@gmail.com>
2012-04-16 22:22:05 +0400

    снова рефакторинг

--- js/jquery.cropbox.js
+++ js/jquery.cropbox.js
@@ -1,6 +1,13 @@
 !(function($){
 
+  /**
+   * Конструктор
+   *
+   * @param element элемент, для которого конструируем
+   * @param options набор опций, смешивается со значениями по дефаулту
+   */
   var CropBox = function(element, options){
+
     this.element = $(element);
     this.options = $.extend({}, $.fn.cropBox.defaults, options);
 
@@ -8,6 +15,7 @@
 
     this.toggle();
     this.setPosition();
+    this.repositeSelected(this.options.width);
 
   };
 
@@ -25,8 +33,12 @@
         height: this.options.height
       });
 
-      this.image.css('width', this.options.width);
-      $(this.image).attr('src', this.element.data('crop'));
+      this.image.css({
+        'width': this.options.width,
+        left: 0,
+        top: 0
+      });
+      this.image.attr('src', this.element.data('crop'));
 
       this.selected.width(this.options.width);
 
@@ -36,8 +48,13 @@
       this.holder.toggle();
     },
 
+    /**
+     * Инициализируем CropBox как синглетон
+     * @return {*}
+     */
     build: function() {
       var holder;
+
       if ( $('#crop-box').length ) {
         holder = $('#crop-box');
 
@@ -50,36 +67,17 @@
           'class': 'b-crop-box',
           id: 'crop-box'
         }).hide().appendTo('body');
-        holder.append('<div class="b-crop-box__area"><img class="b-crop-box__image"/><div class="b-crop-box__selected"/></div><div class="b-crop-box__panel"><div class="b-crop-box__slider"/>"</div>');
+
+        holder.append('<div class="b-crop-box__area"><img class="b-crop-box__image"/><div class="b-crop-box__selected"/></div>' +
+          '<div class="b-crop-box__panel"><div class="b-crop-box__slider"/>"</div>');
 
         this.area = holder.find('.b-crop-box__area');
         this.image = holder.find('.b-crop-box__image');
         this.selected = holder.find('.b-crop-box__selected');
 
-        this.area.on('mousedown', $.proxy(function(e){
-          this.start = {
-            left: e.offsetX,
-            top: e.offsetY
-          };
-
-          $(document).on('mousemove', $.proxy(function(e){
-
-            this.image.css({
-              left: e.clientX - this.start.left - this.holder.position().left,
-              top: e.clientY - this.start.left - this.holder.position().top
-            });
-
-            $(document).on('mouseup', function(){
-              $(document).off('mousemove');
-            });
-
-          }, this));
-
-          e.preventDefault();
-
-        }, this));
-
         this.slider = holder.find('.b-crop-box__slider');
+
+        this.addDragBehaviour();
 
         this.slider.slider({
           slide: $.proxy(this.onSlide, this),
@@ -90,6 +88,34 @@
 
       }
       return(holder);
+    },
+
+    /**
+     * Инициализация D&D поведение для фотографии
+     */
+    addDragBehaviour: function() {
+
+      this.area.on('mousedown', $.proxy(function(e){
+
+        this.start = { left: e.clientX - this.holder.position().left, top: e.clientY - this.holder.position().top };
+
+        $(document).on('mousemove.cropbox', $.proxy(function(e){
+
+          this.image.css({
+            left: e.clientX - this.start.left - this.holder.position().left,
+            top: e.clientY - this.start.top - this.holder.position().top
+          });
+
+        }, this));
+
+        $(document).on('mouseup.cropbox', function(){
+          $(document).off('mousemove.cropbox');
+          $(document).off('mouseup.cropbox');
+        });
+
+        e.preventDefault();
+
+      }, this));
     },
 
     repositeSelected: function(value) {
@@ -104,16 +130,19 @@
     onSlide: function(e, ui) {
       var value = ui.value;
       this.repositeSelected(value);
-    },
+    }
 
   };
 
   $.fn.cropBox = function(options) {
     return this.each(function(){
-      $(this).data('cropBox', (new CropBox(this, options)))
+      $(this).data('crop-box', (new CropBox(this, options)))
     });
   };
 
+  /**
+   * Установки по дефаулту
+   */
   $.fn.cropBox.defaults = {
     width: 260,
     height: 310
